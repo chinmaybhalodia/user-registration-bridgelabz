2bf569e1183df082c736b8217f12a26acdb2abbe
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-12-01 00:32:56 +0530

    added regex check for lastname

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,14 +8,23 @@ public class Main {
 
         // UC1: validating firstname
         String firstNameRegex = "^[A-Z][a-zA-Z]{2,}$";
-        System.out.print("Enter Firstname: ");
+        System.out.print("Enter first name: ");
         String firstName = sc.nextLine();
         while (!firstName.matches(firstNameRegex)) {
             System.out.println("Please enter a valid response.");
-            System.out.print("Enter Firstname: ");
+            System.out.print("Enter first name: ");
             firstName = sc.nextLine();
         }
 
+        // UC2: validating lastname
+        String lastNameRegex = "^[A-Z][a-zA-Z]{2,}$";
+        System.out.print("\nEnter last name: ");
+        String lastName = sc.nextLine();
+        while (!lastName.matches(lastNameRegex)) {
+            System.out.println("Please enter a valid response.");
+            System.out.print("Enter last name: ");
+            lastName = sc.nextLine();
+        }
         sc.close();
     }
 }
