3236558e6648ab43e0d88d877dff9f79b37de1df
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-12-01 00:57:27 +0530

    added regex check for uppercase in password

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -47,7 +47,8 @@ public class Main {
         }
 
         // UC5: validating password (8 chars)
-        String passwordRegex = "^.{8,}$";
+        // UC6: validating password (uppercase)
+        String passwordRegex = "^(?=.*[A-Z]).{8,}$";
         System.out.print("\nEnter password: ");
         String password = sc.nextLine();
         while (!password.matches(passwordRegex)) {
