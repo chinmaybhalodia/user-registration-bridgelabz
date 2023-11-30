9a471b693f99c8dc3501f66eb9a528e784b5019d
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-12-01 00:29:19 +0530

    added regex check for firstname

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("User Registration Problem");
+        Scanner sc = new Scanner(System.in);
+
+        // UC1: validating firstname
+        String firstNameRegex = "^[A-Z][a-zA-Z]{2,}$";
+        System.out.print("Enter Firstname: ");
+        String firstName = sc.nextLine();
+        while (!firstName.matches(firstNameRegex)) {
+            System.out.println("Please enter a valid response.");
+            System.out.print("Enter Firstname: ");
+            firstName = sc.nextLine();
+        }
+
+        sc.close();
     }
 }
