9a71be0afa0c7cba8c1b2e10c655991cb6cc4aaa
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-12-01 01:31:43 +0530

    testing with all given sample emails

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MyTest {
+
+    // UC9: testing with all sample strings
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "abc@yahoo.com",
+            "abc-100@yahoo.com",
+            "abc.100@yahoo.com",
+            "abc111@abc.com",
+            "abc-100@abc.net",
+            "abc.100@abc.com.au",
+            "abc@1.com",
+            "abc@gmail.com.com",
+            "abc+100@gmail.com"
+    })
+    public void testEmails(String email) {
+        String emailRegex = "^[a-zA-Z0-9.$&%_+-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})+$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        assertTrue(matcher.matches());
+    }
+}
